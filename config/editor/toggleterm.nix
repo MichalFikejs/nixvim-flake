3e2efb80b6aba475fac0611236b61f2f18059c80
Michal Fikejs <michal@fikejs.com>
2024-01-14 01:09:49 +0100

    Added some toggleterm mappings

--- config/editor/toggleterm.nix
+++ config/editor/toggleterm.nix
@@ -1,5 +1,30 @@
 {
+  extraConfigLuaPre = ''
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({ 
+      cmd = "lazygit",
+      hidden = true,
+      dir = "git_dir",
+      direction = "float",
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+  '';
   plugins.toggleterm = {
     enable = true;
   };
+  keymaps = [
+    {
+      action = "<cmd>ToggleTerm<CR>";
+      key = "<leader>t";
+      options.desc = "Terminal (root dir)";
+    }
+    {
+      action = "<cmd>lua _lazygit_toggle()<CR>";
+      key = "<leader>gg";
+      options.desc = "LazyGit";
+    }
+  ];
 }
