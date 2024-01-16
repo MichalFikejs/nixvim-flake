97ed7cf6a5b1c5d8864d1add5e406bcefbba4e45
Michal Fikejs <michal@fikejs.com>
2024-01-16 02:18:42 +0100

    Added whichkey mappings

--- config/editor/which-key.nix
+++ config/editor/which-key.nix
@@ -1,5 +1,23 @@
 {
   plugins.which-key = {
     enable = true;
+    plugins.spelling.enabled = true;
+    registrations = {
+      "g" = "+goto";
+      "gs" = "+surround";
+      "]" = "+next";
+      "[" = "+prev";
+      "<leader><tab>" = "+tabs";
+      "<leader>b" = "+buffer";
+      "<leader>c" = "+code";
+      "<leader>f" = "+file/find";
+      "<leader>g" = "+git";
+      "<leader>gh" = "+hunks";
+      "<leader>q" = "+quit/session";
+      "<leader>s" = "+search";
+      "<leader>u" = "+ui";
+      "<leader>w" = "+windows";
+      "<leader>x" = "+diagnostics/quickfix";
+    };
   };
 }
