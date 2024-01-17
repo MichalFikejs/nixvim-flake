474c00cee7a04260dbd4536e32cb54aa4816e8e1
Michal Fikejs <michal@fikejs.com>
2024-01-17 08:22:52 +0100

    Added mini.bufremove

--- config/editor/mini.nix
+++ config/editor/mini.nix
@@ -7,6 +7,7 @@
         symbol = "│";
         options = { try_as_border = true; };
       };
+      bufremove = {};
       comment = {
         custom_commentstring = ''
           function()
@@ -39,4 +40,41 @@
       };
     };
   };
+  keymaps = [
+    {
+      action = ''
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end
+      '';
+      lua = true;
+      key = "<leader>bd";
+      options = {
+        desc = "Delete Buffer";
+      };
+    }
+    {
+      action = ''
+        function() 
+          require("mini.bufremove").delete(0, true)
+        end
+      '';
+      lua = true;
+      key = "<leader>bD";
+      options = {
+        desc = "Delete Buffer (Force)";
+      };
+    }
+  ];
 }
