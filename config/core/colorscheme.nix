360c5f3672be48157d819d9e84f5faceeac70c1d
Michal Fikejs <michal@fikejs.com>
2024-01-31 14:22:48 +0000

    New colors integration

--- config/core/colorscheme.nix
+++ config/core/colorscheme.nix
@@ -3,6 +3,17 @@
     catppuccin = {
       enable = true;
       flavour = "mocha";
+      integrations = {
+        cmp = true;
+        gitsigns = true;
+        treesitter = true;
+        harpoon = true;
+        noice = true;
+        lsp_saga = true;
+        lsp_trouble = true;
+        which_key = true;
+        telescope.enabled = true;
+      };
     }; 
   };
 }
